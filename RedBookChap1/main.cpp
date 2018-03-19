42bde9299a2b52b6630d802913f4977a1334d565
Josiah Blaisdell <josiahblaisdell@outlook.com>
2018-03-18 21:25:35 -0500

    added init and display callback to match the book.

--- RedBookChap1/main.cpp
+++ RedBookChap1/main.cpp
@@ -5,16 +5,9 @@ void display();
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA);
-	glutInitWindowSize(800, 600);
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Chapter 1");
-	//color window is cleared to.
-	glClearColor(0, 0, 0, 0);
-	//clear the window to clear color
-	glClear(GL_COLOR_BUFFER_BIT);
-	//color to use for drawing objects (white).
-	glColor3f(1, 1, 1);
-	//specify the coordinate system, defines how image gets mapped to screen
-	glOrtho(0, 1, 0, 1, -1, 1);
 	//Whenever GLUT determines the contents of the window need to be redisplayed,
 	//the callback function registered by glutDisplayFunc() is executed. Therefore, you should put 
 	//all the routines you need to redraw the scene in the display callback function.
@@ -24,13 +17,27 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
+void init() {
+	//color window is cleared to.
+	glClearColor(0, 0, 0, 0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//specify the coordinate system, defines how image gets mapped to screen
+	glOrtho(0, 1, 0, 1, -1, 1);
+}
+
 void display() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//clear the window to clear color
+	glClear(GL_COLOR_BUFFER_BIT);
+	//color to use for drawing objects (white).
+	glColor3f(1, 1, 1);
+	//draw white polygon at (.25,.25,0) (.75,.75,0)
 	glBegin(GL_POLYGON);
-		glVertex3f(0.25, 0.25, 0.0);
+		glVertex3f(0.25, 0.4, 0.0);
 		glVertex3f(0.75, 0.25, 0.0);
 		glVertex3f(0.75, 0.75, 0.0);
 		glVertex3f(0.25, 0.75, 0.0);
 	glEnd();
+	glutSolidSphere(.75,20,2);
 	glutSwapBuffers();
 }
